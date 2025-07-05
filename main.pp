b2fea5e8bab404190c35434d0ef13b6e5a9861a4
SpaßJulian <spasjulian@gmail.com>
2025-07-05 21:21:50 +0000

    Doorman Implimentiert

---
+++ main.pp
@@ -0,0 +1,11 @@
+VAR
+    Alter  : Integer;
+
+begin;
+    Alter  :=  17;
+    WriteLn(' kommen sie mal her !');
+    if Alter >= 18 then
+        WriteLn('sie dürfen Eintreten !')
+    else
+        WriteLn('Du bist zu Jung und kannst nicht herrein !');
+end.
